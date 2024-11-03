549f380ed135ded427b0a3f82e5400f01e68f639
ahonkazi <aohinuzzaman420@gmail.com>
2024-11-03 15:27:17 +0600

    blog routes added

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,9 @@
 import { Router } from "express";
+import BlogController from "../controller/BlogController";
 const router = Router();
 
+router.post("/create-blog", BlogController.createBlog);
+router.get("/read-blog", BlogController.readBlog);
+router.put("/update-blog", BlogController.updateBlog);
+router.delete("/read-blog", BlogController.deleteBlog);
 export default router;
